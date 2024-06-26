6d83ed9c2155149b9d849652a3dd5ed33e039c2c
NaveenAnuja <anujanaveen83@gmail.com>
2024-06-26 16:51:01 +0530

    first commit

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
-let randomNumber = Math.floor((Math.random()*10)+1);
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+
 console.log(randomNumber);
 
-let attempts = 3;
-function guessnumber(){
-    if (attempts == 0) {
-        alert("Your chances are over.Please Refesh and Replay game..!");
-        return;
-    }
-
-   
-    attempts--;
-
-    const userGuess = document.getElementById('game').value;
-
+        function checkGuess() {
+            let userGuess = document.getElementById('guessInput').value;
+            let message = document.getElementById('message');
             
-            const guess = parseInt(userGuess);
+            let attempts = 3;
 
-           
-            const resultElement = document.getElementById('result');
-
-           
-            if (guess === randomNumber) {
-                resultElement.textContent = 'Congratulations! You guessed the correct number!';
-                resultElement.style.color = 'green';
-            } else if (guess < randomNumber) {
-                resultElement.textContent = ' Try a higher number.';
-                resultElement.style.color = 'red';
-            } else {
-                resultElement.textContent = ' Try a lower number.';
-                resultElement.style.color = 'red';
+              
+            if (userGuess == randomNumber) {
+                message.textContent = "Congratulations! You win the game.";
+                message.style.color = "green";
+            } else if (userGuess < randomNumber) {
+                message.textContent = "Too low! Try again.";
+                message.style.color = "red";
+            } else if (userGuess > randomNumber) {
+                message.textContent = "Too high! Try again.";
+                message.style.color = "red";
             }
+            
+            attempts--;
+        
+
+        if(attempts==0){
+            message.textContent = "Your chances are over.Please Refesh and Replay game..!";
+            message.style.color = "green";
         }
+    }
+        
